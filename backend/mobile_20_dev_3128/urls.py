a3f50f102ec1b1beaec2e390e762782fdc109254
Crowdbotics <team@crowdbotics.com>
2020-04-20 07:24:56 +0000

    Update admin title

--- backend/mobile_20_dev_3128/urls.py
+++ backend/mobile_20_dev_3128/urls.py
@@ -32,9 +32,9 @@ urlpatterns = [
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
 ]
 
-admin.site.site_header = "mobile 20"
-admin.site.site_title = "mobile 20 Admin Portal"
-admin.site.index_title = "mobile 20 Admin"
+admin.site.site_header = "mobile 206"
+admin.site.site_title = "mobile 206 Admin Portal"
+admin.site.index_title = "mobile 206 Admin"
 
 # swagger
 schema_view = get_schema_view(
